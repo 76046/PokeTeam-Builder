753ecc40df3746bcfa911efca53e24c032ed3c04
Justyanna <annajkuczynska@onet.pl>
2022-04-07 22:59:33 +0200

    Add database connection

---
+++ src/methods/db.connection.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const connect = () =>
+  mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+  });
+
+export default connect;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,33 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
 
-import invitationRoutes from './routes/invitation.js'
-import moveRoutes from './routes/move.js'
-import pokemonRoutes from './routes/pokemon.js'
-import summaryRoutes from './routes/summary.js'
-import teamRoutes from './routes/team.js'
-import userRoutes from './routes/user.js'
+import invitationRoutes from "./routes/invitation.js";
+import moveRoutes from "./routes/move.js";
+import pokemonRoutes from "./routes/pokemon.js";
+import summaryRoutes from "./routes/summary.js";
+import teamRoutes from "./routes/team.js";
+import userRoutes from "./routes/user.js";
+import connect from "./methods/db.connection.js";
+import "dotenv/config";
 
-app.use('/invitation', invitationRoutes)
-app.use('/move', moveRoutes)
-app.use('/pokemon', pokemonRoutes)
-app.use('/summary', summaryRoutes)
-app.use('/team', teamRoutes)
-app.use('/user', userRoutes)
+app.use("/invitation", invitationRoutes);
+app.use("/move", moveRoutes);
+app.use("/pokemon", pokemonRoutes);
+app.use("/summary", summaryRoutes);
+app.use("/team", teamRoutes);
+app.use("/user", userRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+connect()
+  .then(() => {
+    console.log("\x1b[42m\x1b[30m%s\x1b[0m", "Database connected");
+    app.listen(port, () => {
+      console.log(
+        "\x1b[44m\x1b[30m%s\x1b[0m",
+        `Example app listening on port ${port}`
+      );
+    });
+  })
+  .catch(() =>
+    console.log("\x1b[42m\x1b[30m%s\x1b[0m", "Database connection failed")
+  );
