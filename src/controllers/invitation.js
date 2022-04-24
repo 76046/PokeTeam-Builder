d4374a0f4c1abd515e685fa79e0edeec98b7dc9c
Justyanna <annajkuczynska@onet.pl>
2022-04-24 17:50:14 +0200

    Add auth checking while accepting invitation

--- src/controllers/invitation.js
+++ src/controllers/invitation.js
@@ -14,6 +14,7 @@ export const getInvitationsById = (req, res) => {
 
 export const getAcceptById = async (req, res) => {
   try {
+    const receiver = req.email;
     const invitation = await Invitation.findOne({ _id: req.params.id })
       .populate("requester", { username: 1, email: 1, _id: 1, friends: 1 })
       .populate("requestee", { username: 1, email: 1, _id: 1, friends: 1 });
@@ -22,6 +23,9 @@ export const getAcceptById = async (req, res) => {
     if (invitation._doc.status === "PENDING") {
       let requester = invitation._doc.requester._doc;
       let requestee = invitation._doc.requestee._doc;
+      if (requestee.email != receiver) {
+        return res.status(401).end("Not authorized");
+      }
       requester.friends.push(requestee._id);
       requestee.friends.push(requester._id);
       await User.findOneAndUpdate(
@@ -36,8 +40,9 @@ export const getAcceptById = async (req, res) => {
         { _id: invitation._doc._id },
         { status: "ACCEPTED" }
       );
+      return res.status(200).end();
     }
-    res.status(200).end();
+    return res.status(404).end("Not found");
   } catch (err) {
     console.error(err);
     res.status(500).end();
--- src/routes/invitation.js
+++ src/routes/invitation.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as controller from "../controllers/invitation.js";
+import auth from "../middleware/auth.js";
 
 const router = Router();
 
@@ -7,7 +8,7 @@ const router = Router();
 
 router.post("/", controller.postInvitation);
 router.get("/invitations", controller.getInvitationsById);
-router.get("/accept/:id", controller.getAcceptById);
+router.get("/accept/:id", auth, controller.getAcceptById);
 router.delete("/:id", controller.deleteInvitationById);
 
 export default router;
